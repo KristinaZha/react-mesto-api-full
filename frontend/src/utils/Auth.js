9bf9effc306be4a0ee67c2380394786ed9af9062
Kristina <kristinka288@gmail.com>
2022-08-23 23:21:24 +0300

    try3

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -43,8 +43,7 @@ export const getContent = (token) => {
       headers: {
          'Accept': 'application/json',
         'Content-Type': 'application/json',
-        Authorization : `Bearer ${token}`,
-      },
+       },
     })
     .then(checkResponse);
   };
